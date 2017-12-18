b51e8038008742bff2008b6279ae3e2074e9b5aa
willcodes <difruscio.dev@gmail.com>
2017-12-17 23:48:27 -0500

    removed old assets

--- core/assets/untitled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="kenney_16x16" tilewidth="16" tileheight="16" tilecount="6912" columns="64">
- <image source="kenney_16x16.png" width="1024" height="1728"/>
-</tileset>
